c79160a36c1e2e1fc3c0bca2f5d9c78870a295b5
Riyan Amanda <ryant.n92@gmail.com>
2025-09-21 18:45:48 +0700

    feat: add more validation rules

--- server/internal/shared/validation/validation.go
+++ server/internal/shared/validation/validation.go
@@ -16,6 +16,32 @@ func Validate[T any](data T) map[string]string {
 			switch v.Tag() {
 			case "required":
 				res[field] = fmt.Sprintf("The field %s is required", field)
+			case "min":
+				res[field] = fmt.Sprintf("The field %s must be at least %s characters", field, v.Param())
+			case "max":
+				res[field] = fmt.Sprintf("The field %s must be at most %s characters", field, v.Param())
+			case "len":
+				res[field] = fmt.Sprintf("The field %s must be exactly %s characters", field, v.Param())
+			case "eq":
+				res[field] = fmt.Sprintf("The field %s must be equal to %s", field, v.Param())
+			case "ne":
+				res[field] = fmt.Sprintf("The field %s must not be equal to %s", field, v.Param())
+			case "gt":
+				res[field] = fmt.Sprintf("The field %s must be greater than %s", field, v.Param())
+			case "gte":
+				res[field] = fmt.Sprintf("The field %s must be greater than or equal to %s", field, v.Param())
+			case "lt":
+				res[field] = fmt.Sprintf("The field %s must be less than %s", field, v.Param())
+			case "lte":
+				res[field] = fmt.Sprintf("The field %s must be less than or equal to %s", field, v.Param())
+			case "email":
+				res[field] = fmt.Sprintf("The field %s must be a valid email address", field)
+			case "url":
+				res[field] = fmt.Sprintf("The field %s must be a valid URL", field)
+			case "uuid":
+				res[field] = fmt.Sprintf("The field %s must be a valid UUID", field)
+			case "oneof":
+				res[field] = fmt.Sprintf("The field %s must be one of [%s]", field, v.Param())
 			default:
 				res[field] = v.Error()
 			}
